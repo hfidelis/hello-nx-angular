bf0ebf5110c5cbda52cff5d1a57f92b51d11fdf1
Heitor Fidelis <heitorc88@gmail.com>
2023-08-29 21:42:04 -0300

    feat: add ui module/lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HelloWorldComponent],
+  declarations: [AppComponent, HelloWorldComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
